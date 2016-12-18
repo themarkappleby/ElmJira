be35a56851432edfcdf4430cb2c2bd8f70fcf72a
themarkappleby <themarkappleby@gmail.com>
2016-12-18 00:42:14 -0500

    Stubbed in elm-css

--- src/Component/Header.elm
+++ src/Component/Header.elm
@@ -1,14 +1,32 @@
 module Component.Header exposing (view)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html exposing (Html)
 import Model exposing (Model)
 import Update exposing (Msg)
+import Css exposing (..)
+import Css.Namespace exposing (namespace)
+import Html.CssHelpers exposing (withNamespace)
+
+
+type CssClass
+    = Header
+
+
+{ id, class, classList } =
+    withNamespace "header"
+
+
+css =
+    (Css.stylesheet << namespace "header")
+        [ (.) Header
+            [ Css.color (Css.hex "CCFFFF")
+            ]
+        ]
 
 
 view : Model -> Html Msg
 view model =
-    header [ class "header" ]
+    Html.header []
         [ viewLeft model
         , viewCenter model
         , viewRight model
@@ -17,14 +35,14 @@ view model =
 
 viewLeft : Model -> Html Msg
 viewLeft model =
-    div [ class "header-left" ] []
+    Html.div [ class [ Header ] ] []
 
 
 viewCenter : Model -> Html Msg
 viewCenter model =
-    div [ class "header-center" ] [ text "Jira Lite" ]
+    Html.div [] [ Html.text "Jira Lite" ]
 
 
 viewRight : Model -> Html Msg
 viewRight model =
-    div [ class "header-right" ] []
+    Html.div [] []
